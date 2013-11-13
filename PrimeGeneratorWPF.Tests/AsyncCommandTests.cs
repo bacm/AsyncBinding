a8748e62b2d0cf7a59457db367b511781ed0e5b3
bacm <bacm@live.co.uk>
2013-11-13 19:42:08 +0100

    starting tests

---
+++ PrimeGeneratorWPF.Tests/AsyncCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace PrimeGeneratorWPF.Tests
+{
+    [TestClass]
+    public class AsyncCommandTests
+    {
+        Mock<AsyncCommand> _command;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _command = new Mock<AsyncCommand>();
+        }
+
+        // todo: find another way to test this without blocking everything if it fails
+        [TestMethod]
+        public void WhenWorkSucceed_ItShouldCallWorkEnded()
+        {
+            var wh = new ManualResetEvent(false);
+
+            _command.Object.RunWorkerCompleted +=
+                (sender, args) => wh.Set();
+
+            _command.Object.Execute(null);
+
+            wh.WaitOne();
+        }
+    }
+}
